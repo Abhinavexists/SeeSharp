b0247ca755d0b2d50f6732f122d9bc83820aa595
Abhinavexists <abhinavkumarsingh2023@gmail.com>
2025-06-30 00:32:47 +0530

    updared requirements.txt

--- gpu.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("CUDA is available!")
-    print(f"GPU Name: {torch.cuda.get_device_name(0)}")
-    print(f"Number of GPUs: {torch.cuda.device_count()}")
-    print(f"Current Device: {torch.cuda.current_device()}")
-else:
-    print("CUDA is NOT available. Using CPU.")
